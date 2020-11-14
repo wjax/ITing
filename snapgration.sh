f64ac89ed43b390fab22591124715008b41409e4
Jesus <wjaxxx@gmail.com>
2020-11-14 15:02:13 +0100

    Updated snapgration and added some useful notes

--- snapgration.sh
+++ snapgration.sh
@@ -1,33 +1,91 @@
-#!/bin/sh
+#!/bin/bash
 
 # This Script creates a new snapshot in the source ZFS dataset and replicate source dataset into destination/backup dataset
 
-if [ $# -ne 3 ]; then
-    echo "Usage: snapgration.sh SRC_Dataset DST_Dataset retention"
-    return 1
+function usage {
+    echo "This script creates a new snapshot in the source ZFS and replicate it into destination/backup ZFS"
+        echo ""
+        echo "usage: snapgration.sh  source destination [-w=time] [-r=retention]"
+    echo "  -w      wait seconds in case another instance is running. 0 if not provided"
+    echo "  -r      snapshot retention as per zfsnap. 1m (month) if not provided"
+    exit 1
+}
+
+
+# Default options
+WAIT="0"
+RETENTION="1m"
+
+# Positional arguments
+SRC="$1"
+DST="$2"
+
+# Number arguments
+ARGV=$#
+
+# Parse arguments
+for i in "$@"
+do
+case $i in
+    -w=*|--wait=*)
+    WAIT="${i#*=}"
+    let "ARGV-=1"
+    shift # past argument=value
+    ;;
+    -r=*|--retention=*)
+    RETENTION="${i#*=}"
+    let "ARGV-=1"
+    shift # past argument=value
+    ;;
+    *)
+          # unknown option
+    ;;
+esac
+done
+
+if [ $ARGV -ne 2 ]; then
+    usage
 fi
 
-src_dataset="$1"
-dst_dataset="$2"
-snapshot_retention="$3"
+# Check if already running and wait
+LOCK_FILE=/var/lock/snapgration.lock
+exec 99>"$LOCK_FILE"
+flock -w $WAIT 99
+RC=$?
+if [ "$RC" != 0 ]; then
+    # Send message and exit
+    echo "Already running script. Exiting"
+    exit 1
+fi
 
-# Create a first snapshot and a full replication only if destination is snapshot empty
-existing_snapshots_dst=$(zfs list -H -t snapshot -o name -S creation -d1 "$dst_dataset")
+echo ""
+echo "Selected options:"
+echo "SRC = $SRC"
+echo "DST = $DST"
+echo "-w = $WAIT"
+echo "-r = $RETENTION"
+echo ""
+echo "Starting ..."
+
+# Do stuff
+
+# Create a first snapshot and a full replication only if destination has no snapshot
+existing_snapshots_dst=$(zfs list -H -t snapshot -o name -S creation -d1 "$DST")
 
 if [ -z "$existing_snapshots_dst" ]; then
     # First replication
     echo "First replication"
-    zfsnap snapshot -rv -a "$snapshot_retention" "$src_dataset"
-    zfs send -R "$(zfs list -H -t snapshot -o name -S creation -d1 "$src_dataset" | head -1)" | zfs recv -Fu "$dst_dataset"
+    zfsnap snapshot -rv -a "$RETENTION" "$SRC"
+    zfs send -R "$(zfs list -H -t snapshot -o name -S creation -d1 "$SRC" | head -1)" | zfs recv -Fu "$DST"
 else
     # Incremental migration
     echo "Incremental replication"
     # Get name of last snapshot in destination dataset
-    last_snapshot_in_destination=$(zfs list -H -t snapshot -o name -S creation -d1 "$dst_dataset" | head -1)
+    last_snapshot_in_destination=$(zfs list -H -t snapshot -o name -S creation -d1 "$DST" | head -1)
     # Transform name to source dataset snapshot name
-    last_replicated_snapshot_in_source="$src_dataset"@${last_snapshot_in_destination#*@}
-    zfsnap snapshot -rv -a "$snapshot_retention" "$src_dataset"
-    latest_snapshot=$(zfs list -H -t snapshot -o name -S creation -d1 "$src_dataset" | head -1)
-    zfs send -RI "$last_replicated_snapshot_in_source" "$latest_snapshot" | zfs recv -Fu "$dst_dataset"
-    zfsnap destroy -rv "$src_dataset"
+    last_replicated_snapshot_in_source="$SRC"@${last_snapshot_in_destination#*@}
+    zfsnap snapshot -rv -a "$RETENTION" "$SRC"
+    latest_snapshot=$(zfs list -H -t snapshot -o name -S creation -d1 "$SRC" | head -1)
+    zfs send -RI "$last_replicated_snapshot_in_source" "$latest_snapshot" | zfs recv -Fu "$DST"
+    zfsnap destroy -rv "$SRC"
 fi
